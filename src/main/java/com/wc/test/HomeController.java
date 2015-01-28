9809b72e01a91e1a77b5aa0441330b4abd39b4ec
choiwonchul <choi.woncheol@gmail.com>
2015-01-28 14:02:16 +0900

    spring test 3

--- src/main/java/com/wc/test/HomeController.java
+++ src/main/java/com/wc/test/HomeController.java
@@ -2,26 +2,37 @@ package com.wc.test;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	
+    @Autowired
+    private SqlSession sqlSession;
+
+	@Resource
+	private MappingJacksonJsonView jsonView;
+    
+    
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -33,7 +44,37 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
+		
+        HashMap<String, String> input = new HashMap<String, String>();
+       input.put("name", "shin");
+       List<HashMap<String, String>> outputs = sqlSession.selectList("userControlMapper.selectSample", input); 
+       System.out.print(outputs.toString());
+				
 		return "home";
 	}
+	
+	
+	@RequestMapping(value = "/init", method = RequestMethod.GET)
+	public String init(Model model) {
+		
+		return "jqgridList";
+	}
+	
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public View testJqGrid(Model model) {
+		
+		
+		//model.addAttribute("serverTime", formattedDate );
+		
+		
+       		
+		return jsonView;
+	}
+	
+	
+	
+	
+	
 	
 }
